05829566d155babf40c3e5b802cb470a7d74ab60
Ins <imbalancerm@gmail.com>
2021-10-09 01:28:56 +0300

    познакомился с массивами в js

--- js/script.js
+++ js/script.js
@@ -15,5 +15,48 @@
 // let greeting = "helo" + currentUserName + '!';
 // document.write(greeting);
 
-let userFirstName = 'Ilya';
-alert(`helo ${'userFirstName'}`);
+// let userFirstName = 'Ilya';
+// alert(`helo ${'userFirstName'}`);
+
+// let currentYear = 2021.6;
+// console.log(`Hi!current year: ${currentYear}`);
+
+// console.log(4 + 3 / -0);
+
+// console.log("lol" - 4);
+
+const person = 'Davidovich';
+// console.log(`hi ${person}`);
+
+
+// let factValue = true;
+// console.log(factValue);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     userName: 'Maximusiys',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+// console.log(obj.userName);
+// console.log(obj['userName']);
+
+
+// let arr = ['cat.png', 'dog.jpeg', 6, 'car.png'];
+// alert(`Zdarova otec ${person}`);
+// const result = confirm('A your here?');
+// console.log(result);
+
+// const answer = prompt('Wat you ages?', '18');
+// console.log(typeof(answer));
+
+const answers = [];
+// answers[0] = prompt('Whats yor name?', '');
+// answers[1] = prompt('Whats yor second name?', '');
+// answers[2] = prompt('Whats yor ages?', '');
+
+console.log(typeof(answers));
