001a0b31cf2ccee4379e2171bb918169fda55cb3
Ins <imbalancerm@gmail.com>
2021-10-08 23:45:44 +0300

    изучение типов данных в JS с практикой

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 'use strict';
 
-let admin;
-let userName = 'Jonnatan';
-admin = userName;
+// let admin;
+// let userName = 'Jonnatan';
+// admin = userName;
 
-console.log(admin);
-
-
-let ourPlannetName;
-let currentUserSiteName;
+// console.log(admin);
 
 
-let currentUserName = prompt('please enter your fckng name');
-let greeting = "helo" + currentUserName + '!';
-document.write(greeting);
+// let ourPlannetName;
+// let currentUserSiteName;
+
+
+// let currentUserName = prompt('please enter your fckng name');
+// let greeting = "helo" + currentUserName + '!';
+// document.write(greeting);
+
+let userFirstName = 'Ilya';
+alert(`helo ${'userFirstName'}`);
