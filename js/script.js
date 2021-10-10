0f499b47e8cd7875d0470b18511dba5b55201c47
Ins <imbalancerm@gmail.com>
2021-10-10 19:31:10 +0300

    понял тему с операторами сравнения и закрепил на практике

--- js/script.js
+++ js/script.js
@@ -5,11 +5,27 @@
 
 // alert(+(a) + +(b)); // 12
 
-(a > b); //больше
+/* (a > b); //больше
 a < b; //меньше
-a >= b; //большеравно
-a <= b; //меньше равно
+a >= b; //больше или равно
+a <= b; //меньше или равно
 a == b; // равно
-a != b; // не равно
+a != b; // не равно */
 
 // операторы сравнения
+
+
+// let art = 5;
+// let tattoo = 6;
+// let copyes = (art != tattoo);
+// console.log(copyes);
+
+// console.log(0 === false);
+
+/* 5 > 4  //true
+"ананас" > "яблоко" //false
+"2" > "12"  //true 
+undefined == null  //true
+undefined === null  //false
+null == "\n0\n"  //false
+null === +"\n0\n"  //false */
