d65a7824253fcac5157d0f0ffb8c12957afdeac9
“Maksym <maksim.riabenko@ribs.pro>
2022-04-12 23:23:05 +0300

    logical operators

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // alert(1);
-let i = 1 + 2;
-console.log(i);
+// let i = 1 + 2;
+// console.log(i);
 
 // let result = confirm('Are you here?');
 // console.log(result);
@@ -19,22 +19,22 @@ console.log(i);
 // console.log(answers);
 
 
-let num = 12;
-let num1 = 3;
+// let num = 12;
+// let num1 = 3;
 
-console.log('+:', num + num1);
-console.log('-:', num - num1);
-console.log('*:', num * num1);
-console.log('/:', num / num1);
+// console.log('+:', num + num1);
+// console.log('-:', num - num1);
+// console.log('*:', num * num1);
+// console.log('/:', num / num1);
 
-let str = 'hello';
-let str1 = 'World';
+// let str = 'hello';
+// let str1 = 'World';
 
-console.log(str + ' ' + str1);
-console.log('Yo' + 16);
+// console.log(str + ' ' + str1);
+// console.log('Yo' + 16);
 
-let inc = 10;
-let dec = 10;  
+// let inc = 10;
+// let dec = 10;  
 
 // ++inc; //інкремент
 // --dec; //декремент
@@ -48,22 +48,51 @@ let dec = 10;
 // console.log(!isClose && isChecked);
 
 
-let result = 12 - 6 / 3;
-let result2 = 3 + 4 * 2;
-let isGreater = 20 - 6 * 3 >= 4;
-let isGreater2 = 20 - 6 * 3 >= 1;
+// let result = 12 - 6 / 3;
+// let result2 = 3 + 4 * 2;
+// let isGreater = 20 - 6 * 3 >= 4;
+// let isGreater2 = 20 - 6 * 3 >= 1;
 
-console.log(isGreater2);
+// console.log(isGreater2);
 
-let e = 1;
-// e = e + 1;
-e += 1;
+// let e = 1;
+// // e = e + 1;
+// e += 1;
 
-e *= 2;
+// e *= 2;
 
-e /= 1.5;
+// e /= 1.5;
 
-console.log(e);
+// console.log(e);
+
+// let num5 = 5;
+// let num17 = 17;
+
+// console.log(num5 <= num17);
+// console.log('3 > 1', 3 > 1);
+
+// console.log('8 >= 7', 8 >= 7);
+// console.log('12 <= 12', 12 <= 12);
+// console.log('4 == 4', 4 == 4);
+// console.log('4 == 5', 4 == 5); 
+
+// console.log('4 !== 4', 4 !== 4);
+// console.log('4 !== 5', 4 !== 5);
+
+// console.log('4 === 4', 4 === 4);
+
+// console.log('4 == "4"', 4 == '4');
+// console.log('4 === "4"', 4 === '4');
+
+console.log(!true || false);
+console.log(true && false);
+
+
+
+
+
+
+
 
 
 
