7449556e67359ae19e900fc7ed0a4796191bdf29
“Maksym <maksim.riabenko@ribs.pro>
2022-04-13 03:01:44 +0300

    first app

---
+++ операторы сравнения/app.js
@@ -0,0 +1,167 @@
+'use strict';
+
+// alert(1);
+// let i = 1 + 2;
+// console.log(i);
+
+// let result = confirm('Are you here?');
+// console.log(result);
+
+// let answer = prompt('What you age?', '17');
+// console.log(typeof answer);
+
+// let answers = [];
+
+// answers[0] = prompt('Как зовут?', '');
+// answers[1] = prompt('Как дела?', '');
+// answers[2] = prompt('го в кино?', '');
+
+// console.log(answers);
+
+
+// let num = 12;
+// let num1 = 3;
+
+// console.log('+:', num + num1);
+// console.log('-:', num - num1);
+// console.log('*:', num * num1);
+// console.log('/:', num / num1);
+
+// let str = 'hello';
+// let str1 = 'World';
+
+// console.log(str + ' ' + str1);
+// console.log('Yo' + 16);
+
+// let inc = 10;
+// let dec = 10;  
+
+// ++inc; //інкремент
+// --dec; //декремент
+
+// console.log(++inc);
+// console.log(--dec);
+
+// let isChecked = true;
+// let isClose = false;
+
+// console.log(!isClose && isChecked);
+
+
+// let result = 12 - 6 / 3;
+// let result2 = 3 + 4 * 2;
+// let isGreater = 20 - 6 * 3 >= 4;
+// let isGreater2 = 20 - 6 * 3 >= 1;
+
+// console.log(isGreater2);
+
+// let e = 1;
+// // e = e + 1;
+// e += 1;
+
+// e *= 2;
+
+// e /= 1.5;
+
+// console.log(e);
+
+// let num5 = 5;
+// let num17 = 17;
+
+// console.log(num5 <= num17);
+// console.log('3 > 1', 3 > 1);
+
+// console.log('8 >= 7', 8 >= 7);
+// console.log('12 <= 12', 12 <= 12);
+// console.log('4 == 4', 4 == 4);
+// console.log('4 == 5', 4 == 5); 
+
+// console.log('4 !== 4', 4 !== 4);
+// console.log('4 !== 5', 4 !== 5);
+
+// console.log('4 === 4', 4 === 4);
+
+// console.log('4 == "4"', 4 == '4');
+// console.log('4 === "4"', 4 === '4');
+
+// console.log(!true || false);
+// console.log(true && false);
+
+let num10 = 10;
+console.log(num10);
+console.log(String(num10));
+
+let value = true;
+console.log(typeof value);
+
+value = String(value);
+console.log(typeof value);
+
+value = Number(value);
+console.log(typeof value);
+
+console.log(typeof ('8' / '2'));
+
+let str4 = 'sting4';
+console.log(typeof str4);
+
+let num = Number(str4);
+console.log(typeof num);
+
+let age = Number('some text 4');
+console.log(age);
+
+let text = '';
+text = Number(text);
+console.log(text);
+
+let x = 1;
+x = -x;
+console.log(x);
+
+x = 10;
+let y = 5;
+console.log(x - y);
+
+let strings = 'my' + ' ' + 'string';
+console.log(strings);
+
+let apples = '2';
+let orange = '6';
+
+console.log(+apples + +orange);
+
+console.log((1 + 5) * 5);
+
+let a, b, c;
+a = b = c = 2 + 5;
+console.log(a, b, c);
+
+let counter = 2;
+console.log(++counter);
+console.log(2 + ++counter);
+
+console.log(true & true);
+
+let t = '1';
+let i = '2';
+console.log(+t + +i);
+console.log(Number(t) + Number(i));
+
+let resultFive = 5 > 1;
+console.log(resultFive);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
