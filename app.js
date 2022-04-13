d084475e4833236209fa1c6ac302d042543165c7
“Maksym <maksim.riabenko@ribs.pro>
2022-04-13 03:02:14 +0300

    update 1st app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '5');
+// let numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '5');
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -11,10 +11,10 @@ let personalMovieDB = {
 
 }
 
-let a = prompt('Один из последних просмотренных фильмов?', 'CODA');
-let b = prompt('На сколько оцените его?', '7.5');
-let c = prompt('Один из последних просмотренных фильмов?', 'CODA');
-let d = prompt('На сколько оцените его?', '7.5');
+// let a = prompt('Один из последних просмотренных фильмов?', 'CODA');
+// let b = prompt('На сколько оцените его?', '7.5');
+// let c = prompt('Один из последних просмотренных фильмов?', 'CODA');
+// let d = prompt('На сколько оцените его?', '7.5');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
