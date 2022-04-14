673a93959b05cea9a0f36b073de22b49bd582232
“Maksym <maksim.riabenko@ribs.pro>
2022-04-14 08:51:29 +0300

    операторы српанения

--- app.js
+++ app.js
@@ -2,26 +2,40 @@
 
 // let numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '5');
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
 
-}
+// }
 
 // let a = prompt('Один из последних просмотренных фильмов?', 'CODA');
 // let b = prompt('На сколько оцените его?', '7.5');
 // let c = prompt('Один из последних просмотренных фильмов?', 'CODA');
 // let d = prompt('На сколько оцените его?', '7.5');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 
-console.log(personalMovieDB.count);
-console.log(personalMovieDB);
+// console.log(personalMovieDB.count);
+// console.log(personalMovieDB);
+
+
+console.log(5 > 4); //true
+console.log("ананас" > "яблоко"); //false
+console.log("2" > "12"); //true
+console.log(undefined == null); //true
+console.log(undefined === null); //false
+console.log(null == "\n0\n"); //false
+console.log(null === +"\n0\n"); //false
+
+
+// console.log(undefined == null);
+
+
 
 
 
